6e657d4ba183eb0cd3c026a1de6d3c0c40dc6acf
Gwen Monteiro <protoos883@gmail.com>
2025-04-01 06:29:36 -0300

    Refactoring/Systems

--- Assets/Scripts/Systems/ActionSystem.cs
+++ Assets/Scripts/Systems/ActionSystem.cs
@@ -1,20 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using System.Collections.Generic;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ActionSystem : Singleton<ActionSystem>
 {
+	#region Fields
+
 	private List<GameAction> reactions = new();
 	public bool IsPerforming { get; private set; } = false;
-	private static Dictionary<Type, List<Action<GameAction>>> preSubs = new();
-	private static Dictionary<Type, List<Action<GameAction>>> postSubs = new();
-	private static Dictionary<Type, Func<GameAction, IEnumerator>> performers = new();
 
-	public void Perform(GameAction action, System.Action OnPerformFinished = null)
+	private static readonly Dictionary<Type, List<Action<GameAction>>> preSubs = new();
+	private static readonly Dictionary<Type, List<Action<GameAction>>> postSubs = new();
+	private static readonly Dictionary<Type, Func<GameAction, IEnumerator>> performers = new();
+
+	#endregion
+
+	#region Public Methods
+
+	/// <summary>
+	/// Performs the given GameAction immediately, executing all pre, performer, and post reactions.
+	/// </summary>
+	/// <param name="action">The GameAction to perform.</param>
+	/// <param name="OnPerformFinished">Optional callback after completion.</param>
+	public void Perform(GameAction action, Action OnPerformFinished = null)
 	{
 		if (IsPerforming) return;
+
 		IsPerforming = true;
 		StartCoroutine(Flow(action, () =>
 		{
@@ -23,11 +35,70 @@ public class ActionSystem : Singleton<ActionSystem>
 		}));
 	}
 
+	/// <summary>
+	/// Adds a GameAction to the reaction queue.
+	/// </summary>
 	public void AddReaction(GameAction action)
 	{
 		reactions ??= new List<GameAction>();
 		reactions.Add(action);
 	}
+
+	/// <summary>
+	/// Attaches a coroutine performer for a specific GameAction type.
+	/// </summary>
+	public static void AttachPerformer<T>(Func<T, IEnumerator> performer) where T : GameAction
+	{
+		Type type = typeof(T);
+		IEnumerator wrappedPerformer(GameAction action) => performer((T)action);
+		performers[type] = wrappedPerformer;
+	}
+
+	/// <summary>
+	/// Detaches the performer for the specified GameAction type.
+	/// </summary>
+	public static void DetachPerformer<T>() where T : GameAction
+	{
+		performers.Remove(typeof(T));
+	}
+
+	/// <summary>
+	/// Subscribes a reaction to be executed at the specified timing for a GameAction.
+	/// </summary>
+	public static void SubscribeReaction<T>(Action<T> reaction, ReactionTiming timing) where T : GameAction
+	{
+		var subs = timing == ReactionTiming.PRE ? preSubs : postSubs;
+
+		void wrapped(GameAction action) => reaction((T)action);
+
+		if (!subs.TryGetValue(typeof(T), out var list))
+		{
+			list = new List<Action<GameAction>>();
+			subs[typeof(T)] = list;
+		}
+
+		list.Add(wrapped);
+	}
+
+	/// <summary>
+	/// Unsubscribes a previously registered reaction.
+	/// </summary>
+	public static void UnsubscribeReaction<T>(Action<T> reaction, ReactionTiming timing) where T : GameAction
+	{
+		var subs = timing == ReactionTiming.PRE ? preSubs : postSubs;
+		if (!subs.TryGetValue(typeof(T), out var list)) return;
+
+		list.RemoveAll(sub =>
+		{
+			// Create a wrapper and compare method info for precise match
+			var method1 = ((Action<GameAction>)((a) => reaction((T)a))).Method;
+			return sub.Method.Equals(method1);
+		});
+	}
+
+	#endregion
+
+	#region Internal Flow Logic
 
 	private IEnumerator Flow(GameAction action, Action OnFlowFinished = null)
 	{
@@ -46,68 +117,26 @@ public class ActionSystem : Singleton<ActionSystem>
 		OnFlowFinished?.Invoke();
 	}
 
+	private IEnumerator PerformReactions()
+	{
+		foreach (var reaction in reactions)
+			yield return Flow(reaction);
+	}
+
 	private IEnumerator PerformPerformer(GameAction action)
 	{
-		Type type = action.GetType();
-		if (performers.TryGetValue(type, out var performer))
-		{
+		if (performers.TryGetValue(action.GetType(), out var performer))
 			yield return performer(action);
-		}
 	}
 
 	private void PerformSubscribers(GameAction action, Dictionary<Type, List<Action<GameAction>>> subs)
 	{
-		Type type = action.GetType();
-		if (subs.TryGetValue(type, out var subscriberList))
+		if (subs.TryGetValue(action.GetType(), out var list))
 		{
-			foreach (var sub in subscriberList)
-			{
+			foreach (var sub in list)
 				sub(action);
-			}
 		}
 	}
 
-	private IEnumerator PerformReactions()
-	{
-		foreach (var reaction in reactions)
-		{
-			yield return Flow(reaction);
-		}
-	}
-
-	public static void AttachPerformer<T>(Func<T, IEnumerator> performer) where T : GameAction
-	{
-		Type type = typeof(T);
-		IEnumerator wrappedPerformer(GameAction action) => performer((T)action);
-		performers[type] = wrappedPerformer;
-	}
-
-	public static void DetachPerformer<T>() where T : GameAction
-	{
-		Type type = typeof(T);
-		performers.Remove(type);
-	}
-
-	public static void SubscribeReaction<T>(Action<T> reaction, ReactionTiming timing) where T : GameAction
-	{
-		Dictionary<Type, List<Action<GameAction>>> subs = timing == ReactionTiming.PRE ? preSubs : postSubs;
-		void wrappedReaction(GameAction action) => reaction((T)action);
-
-		if (!subs.TryGetValue(typeof(T), out var reactionList))
-		{
-			reactionList = new List<Action<GameAction>>();
-			subs[typeof(T)] = reactionList;
-		}
-
-		reactionList.Add(wrappedReaction);
-	}
-
-	public static void UnsubscribeReaction<T>(Action<T> reaction, ReactionTiming timing) where T : GameAction
-	{
-		Dictionary<Type, List<Action<GameAction>>> subs = timing == ReactionTiming.PRE ? preSubs : postSubs;
-		if (subs.TryGetValue(typeof(T), out var reactionList))
-		{
-			reactionList.RemoveAll(sub => sub.Equals((Action<GameAction>)(a => reaction((T)a))));
-		}
-	}
+	#endregion
 }
--- Assets/Scripts/Systems/Singleton.cs
+++ Assets/Scripts/Systems/Singleton.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
 
+/// <summary>
+/// Generic singleton base class for MonoBehaviours.
+/// Ensures only one instance exists at runtime.
+/// </summary>
+/// <typeparam name="T">The type to be used as a singleton.</typeparam>
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
+	#region Public Static Properties
+
+	/// <summary>
+	/// The current instance of the singleton.
+	/// </summary>
 	public static T Instance { get; private set; }
 
+	#endregion
+
+	#region Unity Lifecycle
+
+	/// <summary>
+	/// Initializes the singleton instance or destroys duplicates.
+	/// </summary>
 	protected virtual void Awake()
 	{
 		if (Instance != null && Instance != this)
@@ -11,10 +28,14 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 			Destroy(gameObject);
 			return;
 		}
+
 		Instance = this as T;
 	}
 
 #if UNITY_EDITOR
+	/// <summary>
+	/// Resets the instance on application quit (Editor only).
+	/// </summary>
 	protected virtual void OnApplicationQuit()
 	{
 		Instance = null;
@@ -22,13 +43,25 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 	}
 #endif
 
-	// Corrigido: nome do tipo interno alterado para TPersistent
+	#endregion
+
+	#region Nested Types
+
+	/// <summary>
+	/// A persistent singleton that survives scene loads.
+	/// </summary>
+	/// <typeparam name="TPersistent">Type of persistent singleton.</typeparam>
 	public abstract class PersistentSingleton<TPersistent> : Singleton<TPersistent> where TPersistent : MonoBehaviour
 	{
+		/// <summary>
+		/// Prevents the object from being destroyed on scene load.
+		/// </summary>
 		protected override void Awake()
 		{
 			base.Awake();
 			DontDestroyOnLoad(gameObject);
 		}
 	}
+
+	#endregion
 }
